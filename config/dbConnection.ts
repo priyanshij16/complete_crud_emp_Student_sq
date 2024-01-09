fee8ad3c2d62cef19e3a18c457da1633948f68c4
Priyanshi Jain <91975234+priyanshij16@users.noreply.github.com>
2024-01-09 15:40:49 +0530

    Update dbConnection.ts

--- config/dbConnection.ts
+++ config/dbConnection.ts
@@ -1,51 +1,50 @@
-const Umzug = require('umzug');
-import sequelize from 'sequelize';
-import { Sequelize, DataType } from 'sequelize';
+const { Umzug } = require('umzug');
+const { SequelizeStorage } = require('umzug');
+const { Sequelize } = require('sequelize'); // Import Sequelize itself
+import { log } from 'console';
 import databaseInstance from './db';
 import path from 'path';
-
-// console.log("path", path.join(__dirname, '../migrations'));
-// let pathVal = path.join(__dirname, '../migrations');
-
-const migrate = new Umzug({
+// Create a Sequelize instance using your database connection
+const umzug = new Umzug({
     migrations: {
         // Path of the migration file
-        path : path.join(__dirname, '../migrations'), 
+        path: path.join(__dirname, '../migrations'),
         // The pattern that determines whether or not a file is a migration.
-        pattern: /\.js$/,
-        //The params that gets passed to the migrations. ?
-        params: [databaseInstance.getQueryInterface (), sequelize]
-
-    },
-    storage: "sequelize",
-    storageOptions:{
+        // pattern: /\.js$/,
+        pattern: /^\d+[\w-]+\.js$/,
+        // The params that get passed to the migrations.
+        params: [databaseInstance.getQueryInterface(), Sequelize],
+        glob: 'path/to/migrations/*.js' //>>>>>>>>>>>>>>>> mandotry to include this 
+    }, 
+    storage: new SequelizeStorage({
         sequelize: databaseInstance,
-    }
-})
-
-const connectionDatabase = async () => {
-        try{
-            const connectionResult = await databaseInstance.authenticate()
-            .then(async() => {
-                console.log("Database Connection done successfully")
-
-                await migrate.up()
-                .then(async() => {
-                    console.log("Migration run successfully");
-                }).catch((err: any) => {
-                    console.log("Migration Failed", err);
-                })
-                
-            }).catch((err: any) => {
-                console.log("Error in database connection authentication");
-                
-            }) 
-        }
-        catch(err) {
-            console.log("Failed database connection ");
-            return Promise.reject(err);
-        }
+        modelName: 'student',
+    }),
     
+});
+//console.log("   >>>>>>>>> migrations path : ",path.join(__dirname, '../migrations') , __dirname);
+const connectionDatabase = async () => {
+    try {
+     const connectionResult = await databaseInstance.authenticate()
+           .then(async () => {
+             console.log("Database Connection done successfully");
+                await umzug.up()
+                    .then(async () => {
+                        console.log("Migration run successfully");
+                    })
+                    .catch((err: any) => {
+                        console.log("Migration Failed", err);
+                    });
+                
+            })
+            .catch((err: any) => {
+                console.log("Error in database connection authentication", err);
+                
+            });
+    } catch (err) {
+        console.log("Failed database connection ", err);
+        return Promise.reject(err);
+    }
 }
-export default connectionDatabase;
 
+export default connectionDatabase;
